a3639672ad5cf374290972e80eae37e34f3a25bf
Lpiedagnel <piedagnel.dev@gmail.com>
2023-03-01 12:29:21 +0100

    Add checking on backend for register

--- elements/signup_checker.php
+++ elements/signup_checker.php
@@ -2,29 +2,60 @@
 include_once('config/db.php');
 $pdo = connect_to_database();
 
-if 
-    (isset($_POST) && 
-    (isset($_POST['firstName'])) && 
-    (isset($_POST['lastName'])) &&
-    (isset($_POST['email'])) &&
-    (isset($_POST['password'])) &&
-    (isset($_POST['passwordConfirmation']))
-    ) {
-    // Hash
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT, ['cost' => 12]);
-    // Get data
-    $data = [
-        'first_name' => htmlspecialchars($_POST['firstName']),
-        'last_name' => htmlspecialchars($_POST['lastName']),
-        'email' => htmlspecialchars($_POST['email']),
-        'job' => $_POST['job'] ? htmlspecialchars($_POST['job']) : null,
-        'password' => $password
-    ];
-    // Store to database
-    $query = "INSERT INTO users (first_name, last_name, email, job, password) VALUES (:first_name, :last_name, :email, :job, :password)";
-    $statement = $pdo->prepare($query);
-    $statement->execute($data);
-    echo '<p class="my-5">Vous êtes enregistré(e) !</p>';
+// Store input into variables
+$first_name = htmlspecialchars($_POST['firstName']);
+$last_name = htmlspecialchars($_POST['lastName']);
+$email = htmlspecialchars($_POST['email']);
+$job = $_POST['job'] ? htmlspecialchars($_POST['job']) : null;
+$password = htmlspecialchars($_POST['password']);
+$password_confirmation = htmlspecialchars($_POST['passwordConfirmation']);
+
+
+$error = null;
+// Check if all fields are complete
+if (empty($first_name) || (empty($last_name)) || (empty($email)) || (empty($password)) || (empty($password_confirmation))) {
+    $error = "Vous devez rentrer toutes les valeurs !";
+}
+
+// Check password
+if ($password !== $password_confirmation) {
+    $error = "Les mots de passe doivent êtres identiques !";
+}
+
+// Check email
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $error = "Vous devez rentrer un email valide";
+}
+
+// Password length
+if (strlen($password) <= 4){
+    $error = "Vous devez choisir un mot de passe d'au moins 4 caractères !";
+}
+
+if (!$error) {
+    $checkEmail = $pdo->prepare("SELECT email FROM users WHERE email = :email");
+    $checkEmail->bindParam(':email', $email);
+    $checkEmail->execute();
+
+    if($checkEmail->rowCount() > 0){
+        echo "L'utilisateur existe déjà.";
+    } else {
+        // Hash
+        $password_hash = password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
+        // Get data
+        $data = [
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+            'email' => $email,
+            'job' => $job,
+            'password' => $password_hash
+        ];
+        // Store to database
+        $query = "INSERT INTO users (first_name, last_name, email, job, password) VALUES (:first_name, :last_name, :email, :job, :password)";
+        $statement = $pdo->prepare($query);
+        $statement->execute($data);
+        echo '<p class="my-5">Vous êtes enregistré(e) !</p>';
+    } 
 } else {
-    echo 'Aucune valeur indiquée';
+    echo 'Une erreur s\'est produite : ' . $error;
 }
