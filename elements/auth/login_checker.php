dc80edadf8baf5be38d1d97d62ed38da335973fc
Lucas Piedagnel <piedagnel.dev@gmail.com>
2023-03-03 12:12:45 +0100

    Manage error on login form

--- elements/auth/login_checker.php
+++ elements/auth/login_checker.php
@@ -17,15 +17,15 @@ if (empty($email) || (empty($password))) {
 if (!$error) {
     $result = $pdo->prepare("SELECT * FROM users WHERE email= :email");
     $result->execute(['email' => $email]);
-    $row = $result->fetch();
-    print_r($row['password']);
-    if ($row && password_verify($password, $row['password'])) {
-        $_SESSION['first_name'] = $row['first_name'];
-        $_SESSION['last_name'] = $row['last_name'];
+    $user = $result->fetch();
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['first_name'] = $user['first_name'];
+        $_SESSION['last_name'] = $user['last_name'];
         header("location: index.php");
         exit;
     }
     else{
         $error = 'L\'utilisateur n\'a pas été trouvé. Vérifiez le mot de passe ou l\'adresse mail.';
+        echo '<p class="my-5">' . $error . '</p>';
     }
 }
